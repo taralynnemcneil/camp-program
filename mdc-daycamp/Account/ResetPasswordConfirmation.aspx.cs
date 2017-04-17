a0e9e09a15003851e9cea5e1dce5d0cca2769534
Tara McNeil <taralynnemcneil@gmail.com>
2017-04-17 18:23:57 -0400

    Add project files.

---
+++ mdc-daycamp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace mdc_daycamp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ mdc-daycamp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mdc_daycamp.Startup))]
+namespace mdc_daycamp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
